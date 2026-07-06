c6ffb5db4cdfd7f99b9a91bc3aa70043d254ef97
Khwahish <sindhwani.khwahish@gmail.com>
2026-07-06 14:38:04 +0530

    ✅ Capacity To Ship Packages Within D Days | Medium | Java | Runtime: 0 ms | Memory: 42.3 MB | 2026-07-06

---
+++ leetcode/medium/1011-capacity-to-ship-packages-within-d-days/solution.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int low = Integer.MAX_VALUE;
+        int high = 0 ;
+        for(int weight : weights){
+            low = Math.max(low,weight);
+            high = high + weight;
+        } 
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if (isShipped(weights, days, mid)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
+        }
+        return low;
+        
+    }
+    static boolean isShipped(int [] weights, int days, int load){
+        int currentLoad = 0;
+        int requiredDays = 1; 
+        for (int weight : weights){
+
+            if (currentLoad + weight <= load){
+                currentLoad += weight;
+            }
+            else {
+
+            requiredDays++;
+            currentLoad = weight;
+
+            if (requiredDays > days)
+                return false;
+            }
+        }
+        return true;
+    }
+}
