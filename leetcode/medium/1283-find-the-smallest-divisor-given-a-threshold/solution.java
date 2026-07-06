fff9309cecd5ab31922e7ffad80d89d2a5ece6a0
Khwahish <sindhwani.khwahish@gmail.com>
2026-07-06 13:43:43 +0530

    ✅ Find the Smallest Divisor Given a Threshold | Medium | Java | Runtime: 0 ms | Memory: 42.2 MB | 2026-07-06

---
+++ leetcode/medium/1283-find-the-smallest-divisor-given-a-threshold/solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int smallestDivisor(int[] nums, int threshold) {
+        int start = Integer.MAX_VALUE;
+        int end = Integer.MIN_VALUE;
+        for(int num : nums){
+            start = Math.min(start, num);
+            end = Math.max(end, num);
+        }
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(isDivisor(nums, threshold, mid)){
+                end = mid -1;
+            }
+            else {
+                start = mid +1;
+            }
+        }
+        return start;
+
+    }
+    static boolean isDivisor(int [] nums, int threshold, int divisor){
+
+        int sumResult = 0;
+        for(int num : nums){
+            //celling as there is approx equal to oe greater then
+            sumResult = sumResult + (num + divisor -1)/divisor;
+
+            if (sumResult > threshold){
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+       return false; 
+    }
+}
