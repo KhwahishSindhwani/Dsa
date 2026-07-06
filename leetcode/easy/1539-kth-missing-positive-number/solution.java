e7918116aa57580cfcc8595b899707d9877b49b5
Khwahish <sindhwani.khwahish@gmail.com>
2026-07-06 16:45:19 +0530

    ✅ Kth Missing Positive Number | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-07-06

---
+++ leetcode/easy/1539-kth-missing-positive-number/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+       int num = 1;
+       int n = arr.length;
+       int i =0;
+       while(i<n && k>0){
+
+        if (arr[i] == num){
+            i++;
+        }else{
+            k--;
+        
+        num ++;}
+       } 
+
+       while(k-- == 0){
+        num++;
+       }
+       return num-1;
+    }
+    
+}
