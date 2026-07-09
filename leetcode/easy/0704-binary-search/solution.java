4d2c77a04b17ee73a4e0b0559d95d2ad914a1beb
Khwahish <sindhwani.khwahish@gmail.com>
2026-07-09 23:30:22 +0530

    ✅ Binary Search | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 48.4 MB | 2026-07-09

--- leetcode/easy/0704-binary-search/solution.java
+++ leetcode/easy/0704-binary-search/solution.java
@@ -2,9 +2,7 @@ class Solution {
     public int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length-1;
-        if (nums.length == 0){
-            return -1;
-        }
+        
 
         while(start <= end){
             int mid = start + ( end - start ) / 2;
