08806b4dee90d7fffdb3c75c9433de362376da13
Khwahish <sindhwani.khwahish@gmail.com>
2026-07-16 01:22:32 +0530

    ✅ Find the Duplicate Number | Medium | Java | Runtime: 5 ms (60.89%) | Memory: 82.9 MB | 2026-07-15

--- leetcode/medium/0287-find-the-duplicate-number/solution.java
+++ leetcode/medium/0287-find-the-duplicate-number/solution.java
@@ -1,5 +1,26 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+
+        int i = 0;
+
+        while (i < nums.length) {
+
+            int correct = nums[i] - 1;
+
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                if (i != correct) {
+                    return nums[i];
+                }
+                i++;
+            }
+        }
+
+        return -1;
+    }
+/*class Solution {
+    public int findDuplicate(int[] nums) {
         int i = 0;
         while (i < nums.length){
             if (nums[i] != i+1){
@@ -16,7 +37,7 @@ class Solution {
         }
         
         return -1;
-    }
+    }*/
 /*class Solution {
     public int findDuplicate(int[] nums) {
         int i = 0;
