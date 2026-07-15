04a5d015ed105349ff9cb0d1198a117b52cabd82
Khwahish <sindhwani.khwahish@gmail.com>
2026-07-15 23:37:49 +0530

    ✅ Missing Number | Easy | Java | Runtime: 1 ms (29.87%) | Memory: 47 MB | 2026-07-15

--- leetcode/easy/0268-missing-number/solution.java
+++ leetcode/easy/0268-missing-number/solution.java
@@ -25,3 +25,15 @@ class Solution {
 
     }
 }
+// bit manupution
+/*
+class Solution {
+    public int missingNumber(int[] nums) {
+     int ans = nums.length;
+     for(int i=0;i<nums.length;i++){
+        ans = ans ^ i ^ nums[i];
+     }
+     return ans;
+    }
+}
+*/
