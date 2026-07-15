7d89da535feb60e234dfe5a404470fdfcdaa3803
Khwahish <sindhwani.khwahish@gmail.com>
2026-07-15 23:35:13 +0530

    ✅ Missing Number | Easy | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-07-15

--- leetcode/easy/0268-missing-number/solution.java
+++ leetcode/easy/0268-missing-number/solution.java
@@ -1,19 +1,27 @@
 class Solution {
+
+    void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
     public int missingNumber(int[] nums) {
-        int i =0;
-        int correct;
+        int i = 0;
         while(i < nums.length){
-             correct = i -1;
-             if(nums[i] == 0){
-                break;
-             }
-            if (nums[i] != correct ){
-                return correct;
-            }
-            else{
+            int correct = nums[i];
+            if(nums[i]<nums.length && nums[i] != nums[correct]){
+                swap(nums, i, correct);
+            }else {
                 i++;
             }
         }
-        return -1;
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index) {
+                return index;
+            }
+        }
+
+        return nums.length;
+
     }
 }
