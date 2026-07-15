51f88a87258e5e7e272062400388d852ad73eebf
Khwahish <sindhwani.khwahish@gmail.com>
2026-07-16 01:35:12 +0530

    ✅ Set Mismatch | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-07-15

---
+++ leetcode/easy/0645-set-mismatch/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+         int i = 0;
+        while (i < nums.length){
+            int correct = nums[i] -1;
+            if (nums[i] != nums[correct]){
+                swap(nums, i, correct);
+            }
+            else {
+                i++;
+            }
+        }
+        int [] ans = new int [2];
+        for (int index = 0; index<nums.length; index++){
+            if (nums[index] != index +1){
+                ans[0] = nums[index];
+                ans[1] = index;
+            }
+        }
+        return ans;
+    }
+     void swap (int [] nums, int first , int second ){
+        int temp = nums[first];
+    nums[first] = nums[second];
+    nums[second] = temp;
+    }
+}
