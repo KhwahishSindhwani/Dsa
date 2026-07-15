acb2eaf4ac5a76f6b8f29bceec3c8e7eccbd1aff
Khwahish <sindhwani.khwahish@gmail.com>
2026-07-16 01:45:49 +0530

    ✅ Set Mismatch | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-07-15

--- leetcode/easy/0645-set-mismatch/solution.java
+++ leetcode/easy/0645-set-mismatch/solution.java
@@ -10,14 +10,20 @@ class Solution {
                 i++;
             }
         }
-        int [] ans = new int [2];
+        //int [] ans = new int [2];
         for (int index = 0; index<nums.length; index++){
             if (nums[index] != index +1){
-                ans[0] = nums[index];
-                ans[1] = index+1;
+                //ans[0] = nums[index];
+                //ans[1] = index+1;
+                //ans = {nums[index], index+1};
+                // i can do 
+                // avoid creating an array it will take space
+                 return new int[]{nums[index], index+1};
             }
         }
-        return ans;
+       // return ans;
+        // or if above one retirn is used
+         return new int[]{-1,-1};
     }
      void swap (int [] nums, int first , int second ){
         int temp = nums[first];
